14a655cef04ff77beb6e61dfd91667dd3d87170e
Lucas Apollo <apollohardrock@gmail.com>
2023-05-19 09:59:17 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+
+function updateProfileInfo(profileData) {
+
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name 
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardskills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softskills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+
+}
+
+function updateLanguages(profileData) {
+
+}
+
+function updateCertifications(profileData) {
+
+}
+
+function updateProjects(profileData) {
+
+}
+
+(async () => {
+    const profileData = await fetchProfileData ()
+    updateProfileInfo(profileData)
+    updateHardSkills(profileData)
+    updateSoftSkills(profileData)
+    updateLanguages(profileData)
+    updateCertifications(profileData)
+    updateProjects(profileData)
+})()
